863ebc87c6f7870bff1f906dc76487121694aaa8
anushka4118 <anushka4118@yahoo.com>
2020-06-20 05:07:03 -0400

    Make string repeat

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let newString = '';
+    if (num < 0) return 'ERROR';
+    for (let i=0; i < num; i++) {
+        newString = newString + string;
+    }
+    return newString;
 }
 
-module.exports = repeatString
+module.exports = repeatString;
