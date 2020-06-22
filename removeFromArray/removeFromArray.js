96042501c4494c4d4a15f4609eb9810a1ada93ec
anushka4118 <anushka4118@yahoo.com>
2020-06-22 17:19:11 -0400

    make remoeFromArray work

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let newArray = [];
+    for (var i=0; i < array.length; i++) {
+        var candidate = array[i];
 
+        var hasFailedTest = false
+
+        for (var j=0; j < args.length; j++) {
+            var filterElement = args[j];
+
+            if (candidate === filterElement) {
+                hasFailedTest = true;
+
+                break;
+            }
+        }
+
+        if (!hasFailedTest) {
+            newArray.push(candidate);
+        }
+    }
+
+    return newArray;
 }
 
 module.exports = removeFromArray
