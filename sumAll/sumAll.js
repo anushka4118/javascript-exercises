d0a820f9e99490b21a7f59a1ea819427a12dce25
anushka4118 <anushka4118@yahoo.com>
2020-06-23 16:50:31 -0400

    Update

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    if (min < 0 || max < 0) {
+        return 'ERROR';
+    }
+    if (min > max) {
+        let newMax = min;
+        min = max;
+        max = newMax;
+    }
+    if (typeof min !== 'number' || typeof max !== 'number') {
+        return 'ERROR';
+    }
+    let sum = 0;
+    for (var i=min; i < max + 1; i++) {
+        sum += i;
+    }
+    return sum;
 }
 
 module.exports = sumAll
+ 
