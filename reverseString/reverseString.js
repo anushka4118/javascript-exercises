4d9988001eab5a93b354aa1cefa6f4b82276ffa0
anushka4118 <anushka4118@yahoo.com>
2020-07-03 16:31:31 -0400

    Make string reverse

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let splitString = string.split("");
+    let reverse = splitString.reverse();
+    let joinArray = reverse.join("");
 
+    return joinArray;
 }
 
 module.exports = reverseString
