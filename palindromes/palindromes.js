3002d81355fe85f3aa1de3952c9db8e49baefe2f
anushka4118 <anushka4118@yahoo.com>
2020-07-03 16:29:30 -0400

    Update palindrome function

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function() {
+const palindromes = function(str) {
+    str = str.toLowerCase();
 
+    var newString = '';
+    for (var i = str.length - 1; i >= 0; i--) {
+        if (str[i].match(/[a-z]/i)) { // is a letter (between a - z)
+            newString += str[i];
+        } else {
+            // else remove it from str
+            str = str.replace(str[i], "");
+        }
+    }
+    return newString === str;
 }
 
 module.exports = palindromes
