5c509a5694b042b0ff4152b130b0685c4a67e069
anushka4118 <anushka4118@yahoo.com>
2020-07-03 10:31:38 -0400

    Update functions

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,30 @@
-function add () {
-	
+function add (a, b) {
+	return a + b;
 }
 
-function subtract () {
-	
+function subtract (a, b) {
+	return a - b 
 }
 
-function sum () {
-	
+function sum (arr) {
+	return arr.reduce((total, current) => total + current, 0);
 }
 
-function multiply () {
-	
+function multiply (arr) {
+	return arr.reduce((a, b) => a * b, 1)
 }
 
-function power() {
-	
+function power(a, b) {
+	return Math.pow(a, b);
 }
 
-function factorial() {
-	
+function factorial(n) {
+	if (n === 0) return 1;
+	let product = 1;
+	for (var i=n; i > 0; i--) {
+		product *= i;
+	} 
+	return product;
 }
 
 module.exports = {
