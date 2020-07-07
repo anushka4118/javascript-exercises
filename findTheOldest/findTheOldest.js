ae37408cc792bfdd8b8c93ebcf2472ff21cb84ce
anushka4118 <anushka4118@yahoo.com>
2020-07-07 14:04:24 -0400

    Implement findTheOldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
+    people.sort((a, b) => { 
+        let aLastAlive;
+        let bLastAlive;
 
+        if (a.yearOfDeath) {
+          aLastAlive = a.yearOfDeath;
+        } else {
+            aLastAlive = new Date().getFullYear();
+        }
+
+        if (b.yearOfDeath) {
+            bLastAlive = b.yearOfDeath;
+        } else {
+            bLastAlive = new Date().getFullYear();
+        }
+
+        const previousAge = aLastAlive - a.yearOfBirth;
+        const nextAge = bLastAlive - b.yearOfBirth;
+        return nextAge - previousAge;
+    })  
+    
+    return people[0];
 }
 
 module.exports = findTheOldest
